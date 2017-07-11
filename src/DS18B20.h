2f32b5d0b502ad2e91b93e3ace060b4d8c8c7baf
Scruff.R <scruff.r@sbg.at>
2017-07-11 14:55:10 +0200

    add support for multiple sensors

--- src/DS18B20.h
+++ src/DS18B20.h
@@ -2,6 +2,7 @@
 
 #include <Particle.h>
 #include <OneWire.h>
+#include <math.h>
 
 #define MAX_NAME 8
 #define MAX_RETRIES 3
@@ -14,29 +15,33 @@
 //Parasite Powered or Not
 #define READPOWERSUPPLY 0xB4
 
-class DS18B20{
-  private:
-    OneWire* ds;
-    byte data[12];
-    byte addr[8];
-    byte type_s;
-    byte chiptype;
-    byte _dataCRC;
-    byte _readCRC;
-    bool _singleDrop;
-    char szName[MAX_NAME];
+class DS18B20 {
+private:
+  OneWire* ds;
+  byte _data[12];
+  byte _addr[8];
+  byte _dataCRC;
+  byte _readCRC;
+  bool _singleDrop;
 
-  public:
-    DS18B20(uint16_t pi, bool singleDrop = false);
-    ~DS18B20();
-    boolean search();
-    void resetsearch();
-    void setResolution(uint8_t newResolution);
-    bool readPowerSupply();
-    void getROM(char szROM[]);
-    byte getChipType();
-    char* getChipName();
-    float getTemperature(bool forceSelect = false);
-    float convertToFahrenheit(float celsius);
-    bool crcCheck();
+public:
+  DS18B20(uint16_t pi, bool singleDrop = false);
+  ~DS18B20();
+  boolean search();
+  boolean search(uint8_t addr[8]);
+  void setAddress(uint8_t addr[8]);
+  void resetsearch();
+  void setResolution(uint8_t newResolution);
+  void setResolution(uint8_t addr[8], uint8_t newResolution);
+  bool readPowerSupply();
+  bool readPowerSupply(uint8_t addr[8]);
+  void getROM(char szROM[]);
+  byte getChipType();
+  byte getChipType(uint8_t addr[8]);
+  char* getChipName();
+  char* getChipName(uint8_t addr[8]);
+  float getTemperature(bool forceSelect = false);
+  float getTemperature(uint8_t addr[8]);
+  float convertToFahrenheit(float celsius);
+  bool crcCheck();
 };
