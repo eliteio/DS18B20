ddccb04de506941fcffa81f63a4848313e2e11a0
ScruffR <ScruffR@users.noreply.github.com>
2019-03-09 11:57:11 +0100

    additional checks for scan success

--- src/DS18B20.cpp
+++ src/DS18B20.cpp
@@ -1,26 +1,22 @@
 #include "DS18B20.h"
 
 DS18B20::DS18B20(uint16_t pin, bool singleDrop)
-  : _singleDrop(singleDrop)
 {
-  memset(_data   , 0, sizeof(_data));
-  memset(_addr   , 0, sizeof(_addr));
-  _dataCRC = 
-  _readCRC = 0;
-
-  ds = new OneWire(pin);
+  memset(_data, 0, sizeof(_data));
+  memset(_addr, 0, sizeof(_addr));
+  _dataCRC    = 0; 
+  _readCRC    = 0;
+  _singleDrop = singleDrop;
+  ds          = new OneWire(pin);
 }
 
 boolean DS18B20::search()
 {
-  boolean isSuccess = ds->search(_addr);
-
-  return isSuccess;
+  return search(_addr);
 }
 
 boolean DS18B20::search(uint8_t addr[8])
 {
-
   boolean isSuccess = ds->search(_addr);
   if (isSuccess) memcpy(addr, _addr, 8);
   else           memset(addr, 0, 8);
@@ -42,19 +38,19 @@ void DS18B20::resetsearch()
   ds->reset_search();
 }
 
-void DS18B20::setResolution(uint8_t newResolution)
+bool DS18B20::setResolution(uint8_t newResolution)
 {
-  if (_singleDrop && !_addr[0])
+  if (_singleDrop && _addr[0] == WIRE_UNKNOWN)
   {
     resetsearch();
-    search();
+    if (!search()) return false;
   }
-  setResolution(_addr, newResolution);
+  return setResolution(_addr, newResolution);
 }
 
-void DS18B20::setResolution(uint8_t addr[0], uint8_t newResolution)
+bool DS18B20::setResolution(uint8_t addr[8], uint8_t newResolution)
 {
-  if (!addr[0]) return;
+  if (addr[0] == WIRE_UNKNOWN) return false;
 
   ds->reset();
   ds->select(addr);
@@ -77,21 +73,22 @@ void DS18B20::setResolution(uint8_t addr[0], uint8_t newResolution)
   }
   HAL_Delay_Milliseconds(20);
   ds->reset();
+  return true;
 }
 
 bool DS18B20::readPowerSupply()
 {
-  if (_singleDrop && !_addr[0])
+  if (_singleDrop && _addr[0] == WIRE_UNKNOWN)
   {
     resetsearch();
-    search();
+    if (!search()) return false;
   }
   return readPowerSupply(_addr);
 }
 
 bool DS18B20::readPowerSupply(uint8_t addr[8])
 {
-  if (!addr[0]) return false;
+  if (addr[0] == WIRE_UNKNOWN) return false;
 
   ds->reset();
   ds->select(addr);
@@ -103,23 +100,24 @@ bool DS18B20::readPowerSupply(uint8_t addr[8])
   return ret;
 }
 
-void DS18B20::getROM(char szROM[])
+bool DS18B20::getROM(char szROM[])
 {
-  if (_singleDrop && !_addr[0])
+  if (_singleDrop && _addr[0] == WIRE_UNKNOWN)
   {
     resetsearch();
-    search();
+    if (!search()) return false;
   }
 
   sprintf(szROM, "%02X %02X %02X %02X %02X %02X %02X %02X", _addr[0], _addr[1], _addr[2], _addr[3], _addr[4], _addr[5], _addr[6], _addr[7]);
+  return true;
 }
 
 byte DS18B20::getChipType()
 {
-  if (_singleDrop && !_addr[0])
+  if (_singleDrop && _addr[0] == WIRE_UNKNOWN)
   {
     resetsearch();
-    search();
+    if (!search()) return WIRE_UNKNOWN;
   }
   return getChipType(_addr);
 }
@@ -129,49 +127,51 @@ byte DS18B20::getChipType(uint8_t addr[8])
   return addr[0];
 }
 
-char* DS18B20::getChipName()
+const char* DS18B20::getChipName()
 {
-  if (_singleDrop && !_addr[0])
+  if (_singleDrop && _addr[0] == WIRE_UNKNOWN)
   {
     resetsearch();
-    search();
+    if (!search()) return "not found";
   }
   return getChipName(_addr);
 }
 
-char* DS18B20::getChipName(uint8_t addr[8])
+const char* DS18B20::getChipName(uint8_t addr[8])
 {
   switch (addr[0])
   {
     case 0x10: return "DS18S20"; break;
     case 0x28: return "DS18B20"; break;
     case 0x22: return "DS1822" ; break;
+    case 0x26: return "DS2438" ; break;
     default:   return "Unknown"; break;
   }
 
-  return "";
+  return "impossible";
 }
 
 float DS18B20::getTemperature(bool forceSelect)
 {
-  if (_singleDrop && !_addr[0])
+  if (_singleDrop && _addr[0] == WIRE_UNKNOWN)
   {
     resetsearch();
-    search();
+    if (!search()) return NAN;
   }
-  return getTemperature(forceSelect ? _addr : NULL);
+  return getTemperature(_addr, forceSelect);
 }
 
-float DS18B20::getTemperature(uint8_t addr[8])
+float DS18B20::getTemperature(uint8_t addr[8], bool forceSelect)
 {
-  bool forceSelect = (addr != NULL);
-  if (!_singleDrop && !addr[0]) return NAN;
+  float celsius = NAN;
+  if (!_singleDrop && addr[0] == WIRE_UNKNOWN) return NAN;
 
   ds->reset();
   if (_singleDrop && !forceSelect)
     ds->skip();
   else
     ds->select(addr);
+
   ds->write(0x44);        // start conversion, with parasite power on at the end
   delay(750);     // maybe 750ms is enough, maybe not
                   // we might do a ds.depower() here, but the reset will take care of it.
@@ -180,45 +180,57 @@ float DS18B20::getTemperature(uint8_t addr[8])
     ds->skip();
   else
     ds->select(addr);
+
   ds->write(0xBE);         // Read Scratchpad
+  if (addr[0] == WIRE_DS2438) {
+    ds->write(0x00,0);     // DS2438 requires a page to read
+  }
 
   for (int i = 0; i < 9; i++)
   {           // we need 9 bytes
     _data[i] = ds->read();
-    //Serial.println("DS Read Data:");
-    //Serial.println(data[i], HEX);
-    //Serial.println(" ");
   }
   _dataCRC = (OneWire::crc8(_data, 8));
-  //Serial.println(_dataCRC, HEX);
   _readCRC = (_data[8]);
-  //Serial.println(_readCRC, HEX);
+  if (_dataCRC != _readCRC) return NAN;
 
   // Convert the data to actual temperature
   // because the result is a 16 bit signed integer, it should
   // be stored to an "int16_t" type, which is always 16 bits
   // even when compiled on a 32 bit processor.
-  int16_t raw = (_data[1] << 8) | _data[0];
 
-  if (addr[0] == 0x10)
-  {
-    raw = raw << 3; // 9 bit resolution default
-    if (_data[7] == 0x10)
-    {
-// "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - _data[6];
-    }
+  uint16_t raw;
+  switch(addr[0]) 
+  { // sensor type 
+    case WIRE_DS1820:
+      raw = ((_data[1] << 8) | _data[0]) << 3;          // 9 bit resolution default
+      if (_data[7] == 0x10)                             // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - _data[6];     
+      celsius = (float)raw / 16.0;
+      break;
+
+    case WIRE_DS1822:
+    case WIRE_DS18B20:
+      switch (_data[4] & 0x60)
+      { // lower resolution means shorter conversion time, low bits need masking
+        case 0x00: _data[0] &= ~0b111; break;           //  9 bit  93.75 ms
+        case 0x20: _data[0] &= ~0b011; break;           // 10 bit 187.50 ms
+        case 0x40: _data[0] &= ~0b001; break;           // 11 bit 375.00 ms
+        default: break;                                 // 12 bit 750.00 ms 
+      }
+      celsius = (float)((_data[1] << 8) | _data[0]) / 16.0;
+      break;
+      
+    case WIRE_DS2438:
+      celsius = (float)_data[2] + ((_data[1] >> 3) / 32.0) * (_data[2] & 0x80 ? -1.0 : 1.0);           
+      break;
+      
+    default:
+      celsius = NAN;
+      break;                            
   }
-  else
-  {
-    byte cfg = (_data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-                                          //// default is 12 bit resolution, 750 ms conversion time
-  }
-  return (float)raw / 16.0;
+  
+  return celsius;
 }
 
 float DS18B20::convertToFahrenheit(float celsius)
@@ -228,11 +240,5 @@ float DS18B20::convertToFahrenheit(float celsius)
 
 bool DS18B20::crcCheck()
 {
-  if (_dataCRC != _readCRC)
-  {
-    Serial.println("CRC Failed");
-    return false;
-  }
-
-  return true;
+  return (_dataCRC == _readCRC);
 }
--- src/DS18B20.h
+++ src/DS18B20.h
@@ -4,44 +4,58 @@
 #include <OneWire.h>
 #include <math.h>
 
-#define MAX_NAME 8
-#define MAX_RETRIES 3
+const int MAX_NAME    = 8;
+const int MAX_RETRIES = 3;
 
-// Device resolution
-#define TEMP_9_BIT  0x1F //  9 bit
-#define TEMP_10_BIT 0x3F // 10 bit
-#define TEMP_11_BIT 0x5F // 11 bit
-#define TEMP_12_BIT 0x7F // 12 bit
-//Parasite Powered or Not
-#define READPOWERSUPPLY 0xB4
+// known sensor types
+enum DS18Type  : uint8_t
+{
+  WIRE_UNKNOWN = 0x00,
+  WIRE_DS1820  = 0x10,
+  WIRE_DS1822  = 0x22,
+  WIRE_DS2438  = 0x26,
+  WIRE_DS18B20 = 0x28,
+};
+
+// parasite powered or not
+const uint8_t READPOWERSUPPLY = 0xB4;
+
+// device resolution
+enum DS18res   : uint8_t
+{
+  TEMP_9_BIT   = 0x1F, //  9 bit
+  TEMP_10_BIT  = 0x3F, // 10 bit
+  TEMP_11_BIT  = 0x5F, // 11 bit
+  TEMP_12_BIT  = 0x7F, // 12 bit
+};
 
 class DS18B20 {
 private:
-  OneWire* ds;
-  byte _data[12];
-  byte _addr[8];
-  byte _dataCRC;
-  byte _readCRC;
-  bool _singleDrop;
+  OneWire*     ds;
+  byte         _data[12];
+  byte         _addr[8];
+  byte         _dataCRC;
+  byte         _readCRC;
+  bool         _singleDrop;
 
 public:
   DS18B20(uint16_t pi, bool singleDrop = false);
   ~DS18B20();
-  boolean search();
-  boolean search(uint8_t addr[8]);
-  void setAddress(uint8_t addr[8]);
-  void resetsearch();
-  void setResolution(uint8_t newResolution);
-  void setResolution(uint8_t addr[8], uint8_t newResolution);
-  bool readPowerSupply();
-  bool readPowerSupply(uint8_t addr[8]);
-  void getROM(char szROM[]);
-  byte getChipType();
-  byte getChipType(uint8_t addr[8]);
-  char* getChipName();
-  char* getChipName(uint8_t addr[8]);
-  float getTemperature(bool forceSelect = false);
-  float getTemperature(uint8_t addr[8]);
-  float convertToFahrenheit(float celsius);
-  bool crcCheck();
+  boolean      search();
+  boolean      search(uint8_t addr[8]);
+  void         setAddress(uint8_t addr[8]);
+  void         resetsearch();
+  bool         setResolution(uint8_t newResolution);
+  bool         setResolution(uint8_t addr[8], uint8_t newResolution);
+  bool         readPowerSupply();
+  bool         readPowerSupply(uint8_t addr[8]);
+  bool         getROM(char szROM[]);
+  byte         getChipType();
+  byte         getChipType(uint8_t addr[8]);
+  const char*  getChipName();
+  const char*  getChipName(uint8_t addr[8]);
+  float        getTemperature(bool forceSelect = false);
+  float        getTemperature(uint8_t addr[8], bool forceSelect = false);
+  float        convertToFahrenheit(float celsius);
+  bool         crcCheck();
 };
